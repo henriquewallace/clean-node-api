2ba4613e94443a05da64e6ea04b01587e662e7b4
henriquewallace <henrique.wallace@hotmail.com>
2024-02-02 23:01:19 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,14 +1,17 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
 
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri)
-    this.client.db()
   },
 
   async disconnect (): Promise<void> {
     this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
