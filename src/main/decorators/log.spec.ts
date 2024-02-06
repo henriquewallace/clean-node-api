2004c67e5e5829e021ea0eec2edd76e611c9d9d6
henriquewallace <henrique.wallace@hotmail.com>
2024-02-05 23:52:19 -0300

    refactor: move sut creation to a factory method

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,51 @@
+import { HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { Controller } from './../../presentation/protocols/controller'
+import { LogControllerDecorator } from './log'
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Henrique'
+        }
+      }
+
+      return await new Promise(resolve => { resolve(httpResponse) })
+    }
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut, controllerStub
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const spyHandle = jest.spyOn(controllerStub, 'handle')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(spyHandle).toHaveBeenCalledWith(httpRequest)
+  })
+})
