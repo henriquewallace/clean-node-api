b5c53f3d5575a1a96f7d7ca71cc158026291815c
henriquewallace <henrique.wallace@hotmail.com>
2024-04-01 22:38:17 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -38,7 +38,7 @@ const makeHashComparer = (): HashComparer => {
 const makeTokenGenerator = (): TokenGenerator => {
   class TokenGeneratorStub implements TokenGenerator {
     async generate (id: string): Promise<string> {
-      return await Promise.resolve('any')
+      return await Promise.resolve('any_token')
     }
   }
 
@@ -130,5 +130,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call TokenGenerator with correct id', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
   })
 })
